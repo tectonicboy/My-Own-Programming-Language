1cafef77b885a1f05ce6cc369d3427e11bdb4f25
Kevin Stefanov <kevinstefanov15@gmail.com>
2026-06-23 19:09:36 +0300

    Lexer successfully groups tokens into Code Blocks. Syntax added. Diagram updated.

--- src/frontend/lexer-hirola.cpp
+++ src/frontend/lexer-hirola.cpp
@@ -6,6 +6,7 @@
 #include <iomanip>
 #include <cstdint>
 #include <cctype>
+#include <cstring>
 
 #include "token.h"
 #include "lexer.h"
@@ -23,7 +24,7 @@ __attribute__((always_inline))
 inline void Lexer::lex_newline(void)
 {
     ++current_line_ix;
-    current_col_ix = 0;
+    current_col_ix = 1;
     ++cursor;
 }
 
@@ -71,7 +72,9 @@ __attribute__((always_inline))
 inline void Lexer::lex_num_literal_uint(void)
 {
     current_lexeme_len = 1;
-    for(auto j = cursor + 1; j < source_code_len; ++j){
+    size_t j;
+
+    for(j = cursor + 1; j < source_code_len; ++j){
         if( ! isdigit(source_code[j]) )
             break;
         ++current_lexeme_len;
@@ -79,7 +82,20 @@ inline void Lexer::lex_num_literal_uint(void)
 
     /* Two ways to get here: Either a full token was recorded, or the end of the
      * program was reached. In both cases, simply record the token and move on.
+     *
+     * If program end is reached here, throw a syntax error for not closing
+     * the code block with BLOCK_END keyword.
      */
+
+    if(j == source_code_len)
+    [[unlikely]]
+    {
+        std::cout
+             << "\n\n"
+             << "Syntax Error: Reached end of program without closing the\n"
+             << "              final code block. Use BLOCK_END keyword.\n";
+        std::abort();
+    }
 
     collected_tokens.emplace_back(std::string_view(
                                      (const char*)&(source_code[cursor]),
@@ -97,8 +113,9 @@ inline void Lexer::lex_identifier_and_keyword(void)
 {
     current_lexeme_len   = 1;
     bool keyword_matched = false;
+    size_t j;
 
-    for(size_t j = cursor + 1; j < source_code_len; ++j){
+    for(j = cursor + 1; j < source_code_len; ++j){
         if( !isalpha(source_code[j]) && !(source_code[j] == '_' ))
             break;
         ++current_lexeme_len;
@@ -106,21 +123,75 @@ inline void Lexer::lex_identifier_and_keyword(void)
 
     /* Two ways to get here: Either a full token was recorded, or the end of the
      * program was reached. In both cases, simply record the token and move on.
+     *
+     * If end of program is reached, make sure it was a BLOCK_END keyword
+     * closing the final Code Block and give a syntax error if not.
      */
 
     /* Store the token string view temporarily here. */
     std::string_view temp_identifier_view = std::string_view
         ((const char*)&(source_code[cursor]), current_lexeme_len);
 
+    if(j == source_code_len)
+    [[unlikely]]
+    {
+        if(source_code.compare
+            (cursor,
+             current_lexeme_len,
+             reserved_keyword_strings[KEYWORD_BLOCK_END]) != 0)
+        {
+            std::cout
+                 << "\n\n"
+                 << "Syntax Error: Reached end of program without closing the\n"
+                 << "              final code block. Use BLOCK_END keyword.\n";
+            std::abort();
+        }
+    }
+
     /* Check if it's a language keyword. */
-    for(size_t k = 0; k < reserved_keywords; ++k){
+    for(size_t k = 0; k < total_keywords; ++k){
         if(temp_identifier_view == reserved_keyword_strings[k])
         {
+            /* If the keyword was BLOCK_START or BLOCK_END, update bookkeeping
+             * regarding Code Blocks in the source code.
+             */
+            if(k == KEYWORD_BLOCK_START)
+            {
+                if(inside_code_block == true)
+                {
+                    std::cout
+                         << "\n\n"
+                         << "Syntax Error: Starting a new code block before\n"
+                         << "              having finished the last one.\n"
+                         << "Line: " << current_line_ix << "\n\n";
+                    std::abort();
+                }
+                inside_code_block = true;
+                aux_code_block_directory.emplace_back
+                    (Code_Block(collected_tokens.size(), 0, 0));
+            }
+            else if(k == KEYWORD_BLOCK_END)
+            {
+                if(inside_code_block == false)
+                {
+                    std::cout
+                         << "\n\n"
+                         << "Syntax Error: Finishing a code block that was\n"
+                         << "              never started to begin with.\n"
+                         << "Line: " << current_line_ix << "\n\n";
+                    std::abort();
+                }
+                inside_code_block = false;
+                aux_code_block_directory.back().end_token_index
+                    = collected_tokens.size();
+            }
+
             collected_tokens.emplace_back
                 (std::string_view(temp_identifier_view),
                 current_line_ix,
                 current_col_ix,
                 TOKEN_TYPE_KEYWORD);
+
             keyword_matched = true;
             break;
         }
@@ -145,6 +216,36 @@ void Lexer::Tokenize_Source_Code(void)
 
     for(cursor = 0; cursor < source_code_len; )
     {
+        /* One or more Code Blocks make up the program. Enforce this here and
+         * in the code for reaching the end of the program (so, all token
+         * processor functions for multi-character token types), as well as at
+         * the code for seeing BLOCK_START and BLOCK_END keywords. This check
+         * here only peeks into the source text, not producing tokens.
+         */
+        if(inside_code_block == false)
+        [[unlikely]]
+        {
+            constexpr size_t needed_len =
+                strlen(reserved_keyword_strings[KEYWORD_BLOCK_START]);
+
+            bool next_is_block_start_keyword =
+                   (source_code_len - cursor > needed_len)
+                && (source_code.compare
+                        (cursor,
+                         needed_len,
+                         reserved_keyword_strings[KEYWORD_BLOCK_START]) == 0);
+            if( ! next_is_block_start_keyword){
+                std::cout
+                     << "\n\n"
+                     << "Syntax Error: Source code not inside a Code Block.\n"
+                     << "              Surround all logical parts of your\n"
+                     << "              code with BLOCK_START and BLOCK_END.\n"
+                     << "Line: " << current_line_ix << "\n\n";
+                std::abort();
+            }
+
+        }
+
         /* All these helper function calls are always inlined. */
 
         if (source_code[cursor] == ' ' || source_code[cursor] == '\t')
@@ -176,14 +277,31 @@ void Lexer::Tokenize_Source_Code(void)
 
         else
         {
-            std::cout << "\nError: On line " << current_line_ix + 1 << ":"
-                      << current_col_ix + 1
+            std::cout << "\nError: On line " << current_line_ix << ":"
+                      << current_col_ix
                       << "  --  Unrecognized source code character: "
                       << source_code[cursor]
                       << "\n\n";
             std::abort();
         }
     }
+
+    /* At the end of program, we must have set the End Token Index of the last
+     * seen Code Block to non-zero. If it's still 0 here, BLOCK_END keyword
+     * was never seen for the last Code Block, give a syntax error.
+     *
+     * If it's not seen for a non-last Code Block, this gets caught by the
+     * code for seeing the BLOCK_START keyword.
+     */
+    if(aux_code_block_directory.back().end_token_index == 0)
+    {
+        std::cout
+             << "\n\n"
+             << "Syntax Error: End of program reached and the last Code Block\n"
+             << "              was never closed with BLOCK_END keyword.\n";
+        std::abort();
+    }
+
     return;
 }
 
@@ -192,11 +310,12 @@ int main()
     struct timespec tv1, tv2;
 
     std::string first_program =
+        "BLOCK_START PROGRAM\n"
         "x = 5;\n"
         "base = (x + 105);\n"
         "kk = ( (base * 1000000) - (x * x) );\n"
         "c = (((x) - (base * base)) / (kk) )\n"
-        "PROG_END";
+        "BLOCK_END";
 
     std::cout << "Tokenizing the following program:\n\n"
               << first_program << "\n\n";
@@ -215,6 +334,12 @@ int main()
     }
 
     std::cout << "\nTotal tokens: " << lexer1.collected_tokens.size() << "\n\n";
+
+    std::cout << "Printing collected Code Blocks:\n";
+
+    for(size_t i = 0; i < lexer1.aux_code_block_directory.size(); ++i){
+        lexer1.aux_code_block_directory[i].print_code_block_info();
+    }
 
     std::cout << "Time taken: "
               << ((tv2.tv_nsec - tv1.tv_nsec) / (double)1000.0)
--- src/frontend/lexer.h
+++ src/frontend/lexer.h
@@ -2,48 +2,53 @@
  * multiple lexers consuming different source files in multiple threads at once.
  *
  * Token type can be readily deduced from the first character of the lexeme.
- * If it's a digit, the token is an integer literal.
- * If it's alphabetical or an underscore, the token is an identifier.
- * Identifier tokens promote to KEYWORD type if they match a keyword.
- * The other token types are all one character long: semicolon,
- * an expression operator in { +, -, *, / } and parentheses.
- * All whitespace is ignored.
+ * All whitespace is ignored. No semantic analysis is performed during lexing.
+ * Minimal syntax errors are caught like unrecognized tokens and improper
+ * containment of statements inside Code Blocks.
  *
- * By knowing the type, we know when that token ends, since each token type
+ * Knowing the token type, we know when that token ends, since each token type
  * is described by a non-overlapping set of characters. We know exactly
  * which characters can be right after the last character too. This makes
- * lexing pretty simple to explain and reason about.
+ * lexing simple to reason about and implement.
+ *
+ * The Lexer does not need to check for Code Block types, it just needs to check
+ * for complete code blocks, with START_BLOCK and END_BLOCK, that's all. The
+ * Semantic Analyzer will check Code Block types using the Parser-emitted AST.
  */
 class Lexer
 {
 private:
 
-    uint32_t current_lexeme_len;
-
-    uint32_t current_token_type_ix;
-    uint32_t current_line_ix;
-    uint32_t current_col_ix;
-
-    size_t cursor;
+    uint64_t current_lexeme_len;
+    uint64_t current_token_type_ix;
+    uint64_t current_line_ix;
+    uint64_t current_col_ix;
+    uint64_t current_block_start_token_ix;
+    uint64_t current_block_end_token_ix;
+    uint64_t current_block_type_ix;
+    size_t   cursor;
+    bool     inside_code_block;
 
     const std::string source_code;
-    const size_t source_code_len;
+    const size_t      source_code_len;
 
 public:
 
-    std::vector<Token> collected_tokens;
+    std::vector<Token>      collected_tokens;
+    std::vector<Code_Block> aux_code_block_directory;
 
     /* Notice the usage of std::move(). This is move semantics. Explained in
-     * assets/notes in the C++ notes.
+     * assets/notes/cpp-notes.txt
      */
 
     Lexer(const std::string&& src)
-    : current_line_ix(0), current_col_ix(0),
+    : current_line_ix(1), current_col_ix(1), inside_code_block(false),
       source_code(std::move(src)), source_code_len(source_code.length())
     {
         /* Reserve initial space in the std::vector for 10 thousand tokens. */
         /* This avoids unwanted hidden heap allocations by the vector.      */
         collected_tokens.reserve(10'000);
+        aux_code_block_directory.reserve(100);
     }
 
     void Tokenize_Source_Code(void);
