6c83b399c1ff493553d122f31440243548f94d1c
Kevin Stefanov <kevinstefanov15@gmail.com>
2026-09-02 14:19:42 +0300

    Get started on assembly code generation structures.

--- src/compiler-phases/back-end/x86_64/instruction-selection/x64_basic_instruction_selector.hpp
+++ src/compiler-phases/back-end/x86_64/instruction-selection/x64_basic_instruction_selector.hpp
@@ -1,4 +1,4 @@
-/* The Instruction Selection phase.
+/* Instruction Selection phase.
  *
  * Predefined pattern matching, from IR code to x64 Assembly language code, will
  * dictate what ASM instructions are emitted for a given Hirola source program.
@@ -13,14 +13,61 @@
  * locality-friendly contiguous Memory Arena and a Directory to navigate it,
  * much like how the AST and the SSA IR program are internally stored.
  */
-class Instruction_Selector_x64
+class ASM_Code_Generation_Orchestrator_x64
 {
-    MEM_Arena x64_ASM_Instructions_Arena;
-    ASM_Instructions_Directory x64_ASM_Instructions_Dir;
-    size_t total_asm_insns_emitted;
+public:
+    /* Receives these from an IR Generation Orchestrator: */
+    MEM_Arena IR_instructions_arena;
+    IR_Instructions_Directory IR_instructions_dir;
+
+    /* Brings into existence these new things: */
+    MEM_Arena x64_ASM_instructions_arena;
+    ASM_Instructions_Directory x64_ASM_instructions_dir;
+
+    /* Receives this from the top-level compilation driver: */
+    std::vector<std::vector<size_t>> ASM_code_generation_quotas;
+
+    /* Constructor. */
+    explicit ASM_Generation_Orchestrator
+        (MEM_Arena&& IR_instructions_arena_in,
+         IR_Instructions_Directory&& IR_instructions_dir_in,
+         std::vector<std::vector<size_t>> ASM_code_generation_quotas_in)
+    : IR_instructions_arena(std::move(IR_instructions_arena_in)),
+      IR_instructions_dir(std::move(IR_instructions_dir_in)),
+      x64_ASM_instructions_arena
+        (MEM_Arena(std::string("x64 ASM Instructions Arena"))),
+      x64_ASM_instructions_dir(ASM_Instructions_Directory(0, 0)),
+      ASM_generation_quotas(std::move(ASM_code_generation_quotas_in)) {}
+
+    uint8_t spawn_ASM_code_generator(std::vector<size_t> ASM_code_gen_quota);
+};
+
+class ASM_Code_Generator_x64
+{
+private:
+    /* Receives these from an ASM Code Generation Orchestrator: */
+    MEM_Arena* IR_instructions_arena;
+    IR_Instructions_Directory* IR_instructions_dir;
+    MEM_Areena* x64_ASM_instructions_arena;
+    ASM_Instructions_Directory* x64_ASM_instructions_dir;
+    std::vector<size_t> ASM_code_gen_quota;
+
+public:
+    /* Constructor. */
+    explicit ASM_Code_Generator_x64
+        (MEM_Arena* IR_insn_arena_in,  IR_Instructions_Directory* ir_dir_in,
+         MEM_Arena* x64_insn_arena_in, ASM_Instructions_Directory* asm_dir_in,
+         std::vector<size_t> ASM_code_gen_quota_in)
+    : IR_instructions_arena(IR_insn_arena_in), IR_instructions_dir(ir_dir_in),
+      x64_ASM_instructions_arena(asm_insn_arena_in),
+      x64_ASM_instructions_dir(asm_dir_in),
+      ASM_code_gen_quota(ASM_code_gen_quota_in) {}
+
+    uint8_t generate_ASM_code(void);
+
+private:
+
+    /* One function per IR->ASM code pattern match? */
 
 
-
-
-
-}
+};
