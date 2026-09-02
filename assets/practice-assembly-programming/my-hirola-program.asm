7c6efdc56845eb0a0548a92d07c04b76e784ab0c
Kevin Stefanov <kevinstefanov15@gmail.com>
2026-09-02 18:28:25 +0300

    x86_64 Instruction Selection: Stack frames done.

--- assets/practice-assembly-programming/my-hirola-program.asm
+++ assets/practice-assembly-programming/my-hirola-program.asm
@@ -8,32 +8,59 @@ section .text
 _start:
 
 ; All IR variables and named constants will have a pre-computed RSP offset.
-; So here's a table of the offsets of the IR variables in hirola-program:
-
-; %const_0 :  1 * 8 =   8
-; %a_1     :  2 * 8 =  16
-; %const_1 :  3 * 8 =  24
-; %b_1     :  4 * 8 =  32
-; %const_2 :  5 * 8 =  40
-; %c_1     :  6 * 8 =  48
-; %c_2     :  7 * 8 =  56
-; %const_3 :  8 * 8 =  64
-; %a_2     :  9 * 8 =  72
-; %b_2     : 10 * 8 =  80
-; %d_1     : 11 * 8 =  88
-; %c_3     : 12 * 8 =  96
-; %a_3     : 13 * 8 = 104
-; %const_4 : 14 * 8 = 112
-; %_temp_0 : 15 * 8 = 120
-; %_temp_1 : 16 * 8 = 128
-; %_temp_2 : 17 * 8 = 136
-; %const_5 : 18 * 8 = 144
-; %_temp_3 : 19 * 8 = 152
-; %a_4     : 20 * 8 = 160
+; So here's a table of the offsets of the IR variables in hirola-program.
+; RSP + (TOTAL_STACK_SPACE - OFFSET) is what's used to access a stack variable.
+;
+; OFFSETS:
+;
+; %const_0 :  1 * 8 =   8   OFFSET: 152
+; %a_1     :  2 * 8 =  16   OFFSET: 144
+; %const_1 :  3 * 8 =  24   OFFSET: 136
+; %b_1     :  4 * 8 =  32   OFFSET: 128
+; %const_2 :  5 * 8 =  40   OFFSET: 120
+; %c_1     :  6 * 8 =  48   OFFSET: 112
+; %c_2     :  7 * 8 =  56   OFFSET: 104
+; %const_3 :  8 * 8 =  64   OFFSET: 96
+; %a_2     :  9 * 8 =  72   OFFSET: 88
+; %b_2     : 10 * 8 =  80   OFFSET: 80
+; %d_1     : 11 * 8 =  88   OFFSET: 72
+; %c_3     : 12 * 8 =  96   OFFSET: 64
+; %a_3     : 13 * 8 = 104   OFFSET: 56
+; %const_4 : 14 * 8 = 112   OFFSET: 48
+; %_temp_0 : 15 * 8 = 120   OFFSET: 40
+; %_temp_1 : 16 * 8 = 128   OFFSET: 32
+; %_temp_2 : 17 * 8 = 136   OFFSET: 24
+; %const_5 : 18 * 8 = 144   OFFSET: 16
+; %_temp_3 : 19 * 8 = 152   OFFSET: 8
+; %a_4     : 20 * 8 = 160   OFFSET: 0
 ;-------------------------
 ; TOTAL STACK SPACE: 160 B
 ;-------------------------
-
+;
+; Stack frame allocation algorithm:
+;
+; 1. Set TOTAL_STACK_SPACE = 0
+; 2. Start at the END of the IR Instructions Directory.
+;
+; /* If it's an IR instruction that assigns to a new IR variable: */
+;
+; 3. IF IR_dir_entry->which_ir_instruction ==    IR_INSN_EQUATE or IR_INSN_ADD
+;                                             or IR_INSN_SUB    or IR_INSN_MUL
+;                                             or IR_INSN_DIV
+;    THEN:
+;
+;           for(std::unordered_map<std::string, size_t>::iterator it: my_map)
+;               it.second += 8;
+;
+;           my_map.emplace(insn.lhs_operand, 0);
+;
+; 4. Proceed to previous entry in IR Instructions Directory.
+;
+;
+;
+;
+;
+;
 sub rsp, 160
 
 ; %const_0 = 5
--- src/compiler-phases/back-end/x86_64/instruction-selection/x64_basic_instruction_selector.hpp
+++ src/compiler-phases/back-end/x86_64/instruction-selection/x64_basic_instruction_selector.hpp
@@ -53,6 +53,11 @@ private:
     std::vector<size_t> ASM_code_gen_quota;
 
 public:
+    /* IR variable names map to their current offset from RSP. */
+    std::unordered_map<std::string, size_t> IR_vars_stack_offsets;
+
+    size_t curr_func_total_stack_allocated;
+
     /* Constructor. */
     explicit ASM_Code_Generator_x64
         (MEM_Arena* IR_insn_arena_in,  IR_Instructions_Directory* ir_dir_in,
@@ -67,7 +72,112 @@ public:
 
 private:
 
+    void setup_new_stack_frame(void);
+
     /* One function per IR->ASM code pattern match? */
 
+    /* IR --> ASM  CODE PATTERNS
+     *
+     *==========================================================================
+     *==========================================================================
+     *
+     * Pattern 1:   { %const_0 = 5 }   --->   IR_Variable = literal uint64_t.
+     *
+     * STEPS:
+     *
+     * 1. Store the literal in register rbp.    |  mov rbp, 5
+     * 3. Store register rbp in [rsp + offset]. |  mov [rsp], rbp
+     *--------------------------------------------------------------------------
+     *
+     *==========================================================================
+     *==========================================================================
+     *
+     * Pattern 2:   { %a_1 = %const_0 }   --->   IR_Variable_1 = IR_Variable_2.
+     *
+     * STEPS:
+     *
+     * 1. Store var_2 in rbp from stack offset.  | mov rbp, [rsp + offset2]
+     * 3. Store register rbp in its stack slot.  | mov [rsp + offset1], rbp
+     *--------------------------------------------------------------------------
+     *
+     * Pattern 3:   { %c_3 = %a_2 + %const_0 } ---> IR_var1 = IR_var2 + IR_var3.
+     *
+     * STEPS:
+     *
+     * 1. Store var2 in rbp from its stack offset.   | mov rbp, [rsp + offset2]
+     * 2. Store var3 in rbx from its stack offset.   | mov rbx, [rsp + offset3]
+     * 3. Add rbx into rbp.                          | add rbp, rbx
+     * 4. Store register rbp in var1's stack offset. | mov [rsp + offset1], rbp
+     *--------------------------------------------------------------------------
+     *
+     */
 
 };
+
+void ASM_Code_Generator_x64::setup_new_stack_frame(void)
+{
+    size_t   IR_insn_type;
+    size_t   IR_insn_arena_offset;
+    uint8_t* IR_insn_addr;
+    std::unordered_map<std::string, size_t>::iterator it;
+
+    /* Each IR instruction kind that assigns to a new IR variable. */
+    union
+    {
+        ir_insn_div*    ir_insn_div_ptr;
+        ir_insn_mul*    ir_insn_mul_ptr;
+        ir_insn_sub*    ir_insn_sub_ptr;
+        ir_insn_add*    ir_insn_add_ptr;
+        ir_insn_equate* ir_insn_equ_ptr;
+    };
+
+    /* For each IR instruction that assigns to a new SSA IR variable:
+     *   - Go over all IR variables already present on a stack frame slot,
+     *     increase their offset from RSP by 8.
+     *   - New IR variable's offset from RSP is current TOTAL_STACK_ALLOCATED.
+     *   - Add 8 to TOTAL_STACK_ALLOCATED.
+     */
+    for(size_t i = 0; i < IR_instructions_dir->size(); ++i)
+    {
+        IR_insn_type = (*IR_instructions_dir)[i].which_ir_instruction;
+        IR_insn_arean_offset = (*IR_instructions_dir)[i].ir_insn_arena_offset;
+        IR_insn_addr = IR_instructions_arena->arena_ptr + IR_insn_arena_offset;
+
+        if(   IR_insn_type == IR_INSN_EQUATE || IR_insn_type == IR_INSN_ADD
+           || IR_insn_type == IR_INSN_SUB    || IR_insn_type == IR_INSN_MUL
+           || IR_insn_type == IR_INSN_DIV) [[likely]]
+        {
+            curr_func_total_stack_allocated += 8;
+
+            for(it: IR_vars_stack_offsets)
+                it.second += 8;
+
+            if(IR_insn_type == IR_INSN_EQUATE)
+            {
+                ir_insn_equ_ptr = (ir_insn_equate*)(IR_insn_addr);
+                IR_vars_stack_offsets.emplace(ir_insn_equ_ptr->lhs, 0);
+            }
+            else if(IR_insn_type == IR_INSN_ADD)
+            {
+                ir_insn_add_ptr = (ir_insn_add*)(IR_insn_addr);
+                IR_vars_stack_offsets.emplace(ir_insn_add_ptr->target, 0);
+            }
+            else if(IR_insn_type == IR_INSN_SUB)
+            {
+                ir_insn_sub_ptr = (ir_insn_sub*)(IR_insn_addr);
+                IR_vars_stack_offsets.emplace(ir_insn_sub_ptr->target, 0);
+            }
+            else if(IR_insn_type == IR_INSN_MUL)
+            {
+                ir_insn_mul_ptr = (ir_insn_mul*)(IR_insn_addr);
+                IR_vars_stack_offsets.emplace(ir_insn_mul_ptr->target, 0);
+            }
+            else if(IR_insn_type == IR_INSN_DIV)
+            {
+                ir_insn_div_ptr = (ir_insn_div*)(IR_insn_addr);
+                IR_vars_stack_offsets.emplace(ir_insn_div_ptr->quotient, 0);
+            }
+        }
+    }
+    return;
+}
